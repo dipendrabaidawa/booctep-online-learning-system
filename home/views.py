cc59002ec708ab432979c4f0462cede2e72e37fd
dipendrabaidawa <baidawardipendar@gmail.com>
2021-11-15 10:32:15 +0000

    update the domain in password reset

--- home/views.py
+++ home/views.py
@@ -3684,10 +3684,10 @@ def deleteCourse(request):
 
 def sendResetPasswordEmail(request):
     email = request.POST.get('email')
-
+    
     connection = get_connection()  # uses SMTP server specified in settings.py
     connection.open()  # If you don't open the connection manually, Django will automatically open, then tear down the connection in msg.send()
-
+    
     text = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
     text += '<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">'
     text += '<head><!--[if gte mso 9]><xml><o:OfficeDocumentSettings><o:AllowPNG/><o:PixelsPerInch>96</o:PixelsPerInch></o:OfficeDocumentSettings></xml><![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="x-apple-disable-message-reformatting"><!--[if !mso]><!-->'
@@ -3741,10 +3741,11 @@ def resetPassword(request):
     hashval = hashlib.md5(email.encode())
     hashString = str(hashval.hexdigest())
     lang = getLanguage(request)[0]
+    domain = request.META['HTTP_HOST']
     if lang == 'ar/':
-        link = settings.BASE_URL + "/" + lang + "forgot_password/?param=" + hashString
+        link = "http://" + domain + "/" + lang + "forgot_password/?param=" + hashString
     else:
-        link = settings.BASE_URL + "/en/forgot_password/?param=" + hashString
+        link = "http://" + domain + "/en/forgot_password/?param=" + hashString
     print("lang link ----------------------------------------------------------------------------- ", link)
     try:
         obj = User.objects.get(email=email)
