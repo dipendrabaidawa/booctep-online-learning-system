52d265f07a4d9ca06ba4de7b03c253e30c1655da
frontexpert <kopchikvlada@mail.ru>
2021-12-08 23:06:25 +0900

    vladi

--- booctop/settings.py
+++ booctop/settings.py
@@ -131,54 +131,20 @@ WSGI_APPLICATION = 'booctop.wsgi.application'
 # If you want to use local db, change this config and don't commit to repo.
 DATABASES = {
    'default': {
-<<<<<<< HEAD
-      'ENGINE': 'django.db.backends.mysql',
-      'NAME': 'booctop',
-      'USER': 'root', # 'root',
-      'PASSWORD': '',
-      'HOST': 'localhost', #'localhost',   # Or an IP Address that your DB is hosted on
-      'PORT': '3306',
-    }
-   # 'default': {
-   #      'ENGINE': 'django.db.backends.mysql',
-   #      'NAME': 'dzjq9t85dqv8kckj',
-   #      'USER': 'otp2j74kv3w9g03y', # 'root',
-   #      'PASSWORD': 'ow5vd8s2bue9y9xt',
-   #      'HOST': 'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', #'localhost',   # Or an IP Address that your DB is hosted on
-   #      'PORT': '3306',
-   #  }
-=======
-<<<<<<< HEAD
-      'ENGINE': 'django.db.backends.mysql',
-      'NAME': 'booctop',
-      'USER': 'root',
-      'PASSWORD': '',
-      'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-      'PORT': '3306',
-  }
-
-  # 'default': {
-  #       'ENGINE': 'django.db.backends.mysql',
-  #       'NAME': 'booctop',
-  #       'USER':'root',
-  #       'PASSWORD':'root',
-  #       'HOST':'127.0.0.1',
-  #       'PORT':'8889',
-  #   'OPTIONS': {
-  #       'charset': 'utf8mb4',
-  #       'use_unicode': True,
-  #     }
-  # }
-=======
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dzjq9t85dqv8kckj',
-        'USER': 'otp2j74kv3w9g03y', # 'root',
-        'PASSWORD': 'ow5vd8s2bue9y9xt',
-        'HOST': 'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', #'localhost',   # Or an IP Address that your DB is hosted on
+        'NAME': 'booctop',
+        'USER': 'root', # 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost', #'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'dzjq9t85dqv8kckj',
+        # 'USER': 'otp2j74kv3w9g03y', # 'root',
+        # 'PASSWORD': 'ow5vd8s2bue9y9xt',
+        # 'HOST': 'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', #'localhost',   # Or an IP Address that your DB is hosted on
+        # 'PORT': '3306',
     }
->>>>>>> 1d806aa1ae6b5bb9d62af46657057fb394c03bc1
->>>>>>> 324f7a308c1fa9cc79eaba04f503b071dc661195
 }
 
 # Password validation
